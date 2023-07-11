37dc04a381615767b800d8b0fdcab279a3b943ab
xxboomstick <ddchang1iz@gmail.com>
2023-07-10 23:15:25 -0400

    work dropdown and year font

--- scripting.js
+++ scripting.js
@@ -1,18 +1,6 @@
-var active_section = document.getElementsByClassName("active")[0];
-let contact_button = document.getElementById("to-contact");
-let return_button = document.getElementById("return-btn");
-console.log(return_button);
+// home
 
-function load(target) {
-  active_section.classList.toggle("active");
-  target.classList.toggle("active");
-  active_section = target;
-  if (active_section.id == "home") {
-    return_button.style.visibility = "hidden";
-  }
-  else {
-    return_button.style.visibility = "visible";
-  }
+function opendrop() {
+  document.getElementById("nav-drop-btn").classList.toggle("active");
+  document.getElementById("nav-dropdown").classList.toggle("active");
 }
-
-load(home);
